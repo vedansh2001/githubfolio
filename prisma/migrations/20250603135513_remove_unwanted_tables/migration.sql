693b995ce567722f77af93d292e55e39802e3cd2
Vedansh Mishra <vedanshm2001@gmail.com>
2025-06-03 19:29:17 +0530

    Refractor: remove unwanted tables in Prisma Schema

---
+++ prisma/migrations/20250603135513_remove_unwanted_tables/migration.sql
@@ -0,0 +1,39 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Authenticator` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `SelectedRepo` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `VerificationToken` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Authenticator" DROP CONSTRAINT "Authenticator_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "SelectedRepo" DROP CONSTRAINT "SelectedRepo_repositoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "SelectedRepo" DROP CONSTRAINT "SelectedRepo_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropTable
+DROP TABLE "Account";
+
+-- DropTable
+DROP TABLE "Authenticator";
+
+-- DropTable
+DROP TABLE "SelectedRepo";
+
+-- DropTable
+DROP TABLE "Session";
+
+-- DropTable
+DROP TABLE "VerificationToken";
